ba13f81def8435b1c8d3ab7cd6b58646ef67e7c9
nhatminh <tnminh1997123@gmail.com>
2020-09-25 09:53:11 +0700

    commit 2

---
+++ test/src/test/edit.java
@@ -0,0 +1,8 @@
+package test;
+
+public class edit {
+private void sysout() {
+	// TODO Auto-generated method stub
+
+}
+}
--- test/src/test/main.java
+++ test/src/test/main.java
@@ -2,4 +2,9 @@ package test;
 
 public class main {
 private String name="nhat minh";
+private void sysout() {
+	System.out.println(name);
+	System.out.println();
+
+}
 }
